1f3716cc13777b051012cff6c74ff9faaf332d5d
Larkin <55812754+lpo4576@users.noreply.github.com>
2021-04-23 13:07:50 -0500

    Uploaded initial version

---
+++ Check-lysates.ps1
@@ -0,0 +1,38 @@
+﻿$prefills = Get-Content -Path C:\Users\312127\Desktop\Prefill_targets\*.pre
+$lysates = Get-Content -Path C:\Users\312127\Desktop\Prefill_targets\*.lys
+[string[]]$samples = $null
+[string[]]$lyssamples = $null
+
+
+foreach ($line in $prefills) {
+    if ($line -like "*<tube>*")
+        {$samples += $line}
+    }
+
+$firstpart = $samples -replace "<Tube>", ''
+$secondpart = $firstpart -replace "</Tube>", ''
+$thirdpart = $secondpart -replace ' ', ''
+
+foreach ($line in $lysates) {
+    if ($line -like "*LYS*")
+        {$lyssamples += $line}
+    }
+
+$lfirstpart = $lyssamples -split '"'
+
+[string[]]$lyssamples = $null
+foreach ($line in $lfirstpart) {
+    if ($line -like "LYS*") {
+        $lyssamples += $line
+        }
+    }
+
+$counter = 0
+[string[]]$missing = ''
+
+foreach ($item in $lyssamples) {
+    if ($thirdpart -contains $item) {
+        $counter++
+        }
+    else {$missing += $item}
+    }
